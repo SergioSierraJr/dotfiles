b216a09fb67901024d78e081a4ce54e485aee9f0
Sergio Sierra <sergiosierrajr@outlook.com>
2024-12-14 12:24:22 -0600

    added basic file configurations for fish and kitty.

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_prompt 
+    /home/sergio/Repositories/ss_prompt/target/release/ss_prompt
+end
