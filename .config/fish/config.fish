784780ce812229485100ddad0bb9026d28da52b9
Sergio Sierra <sergiosierrajr@outlook.com>
2025-06-10 00:09:12 -0500

    Modified fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,8 @@ end
 function fish_prompt 
     /home/sergio/Repositories/ss_prompt/target/release/ss_prompt
 end
+
+alias ls="exa --icons=always -T -L 1"
+alias cat="bat"
+alias icat="kitten icat"
+alias ssh="kitten ssh"
